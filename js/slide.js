1ba2c2742618c0c6883c6258d341631a790df1e9
dayeon <dykang@ui-lab.co.kr>
2021-12-17 17:06:37 +0900

    no message

--- js/slide.js
+++ js/slide.js
@@ -1,25 +1,30 @@
 // 슬라이드 스크립트
 $('.slide-1').click(function(){
-    $('.slide-container').removeClass('transforming');
-    $('.slide-container').css('transform','translateX(0vw)');
-    $('.slide-container').addClass('transforming');
+    cssPlus(0)
 });
 $('.slide-2').click(function(){
-    $('.slide-container').removeClass('transforming');
-    $('.slide-container').css('transform','translateX(-100vw)');
-    $('.slide-container').addClass('transforming');
+    cssPlus(100)
 });
 $('.slide-3').click(function(){
-    $('.slide-container').removeClass('transforming');
-    $('.slide-container').css('transform','translateX(-200vw)');
-    $('.slide-container').addClass('transforming');
+    cssPlus(200)
 });
 
-var 지금보이는사진 = 1;
-
-$('.next').click(function(){
+//pagination클릭시함수
+function cssPlus(vw){
+    $('.slide-container').removeClass('transforming');
+    $('.slide-container').css('transform','translateX(-'+ vw +'vw)');
+    $('.slide-container').addClass('transforming');
+};
+//transform추가함수
+function addTransforming(){
     $('.slide-container').removeClass('transforming');
     $('.slide-container').addClass('transforming');
+};
+
+let 지금보이는사진 = 1;
+
+$('.next').click(function(){
+    addTransforming();
     $('.slide-container').css('transform','translateX(-'+지금보이는사진+'00vw)');
     지금보이는사진 = 지금보이는사진 + 1;
     if(지금보이는사진 >= 3){
@@ -29,8 +34,7 @@ $('.next').click(function(){
     }   
 });
 $('.prev').click(function(){
-    $('.slide-container').removeClass('transforming');
-    $('.slide-container').addClass('transforming');
+    addTransforming();
     $('.slide-container').css('transform','translateX(-'+ (지금보이는사진 - 1) +'00vw)');
     if(지금보이는사진 > 1){
         지금보이는사진 = 지금보이는사진 - 1;
@@ -49,14 +53,14 @@ var 매니저3 = new Hammer.Manager(이미지3);
 매니저2.add(new Hammer.Pan({ threshold: 0 }));
 매니저3.add(new Hammer.Pan({ threshold: 0 }));
 
-// var touch = function(e){
+// function touch(e,v){
 //     if( e.deltaX < -100){
 //         $('.slide-container').css('transform','translateX('+ e.deltaX +'px)');
 
 //         //만약에 마우스를 놓으면 두번째 사진으로 변해야함
 //         if(e.isFinal){
 //             $('.slide-container').addClass('transforming');
-//             $('.slide-container').css('transform','translateX(-100vw)');
+//             $('.slide-container').css('transform','translateX(-'+ v +'vw)');
 //             setTimeout(function(){
 //                 $('.slide-container').removeClass('transforming');
 //             }, 10000);
@@ -65,8 +69,7 @@ var 매니저3 = new Hammer.Manager(이미지3);
 // }
 
 매니저.on('pan', function(e){
-    // console.log(e.deltaX);
-    //만약 그림을 왼쪽으로 슬라이드 했을때 오른쪽그림 보여줘
+    지금보이는사진 = 1
     if( e.deltaX < -100){
         $('.slide-container').css('transform','translateX('+ e.deltaX +'px)');
 
@@ -79,8 +82,10 @@ var 매니저3 = new Hammer.Manager(이미지3);
             }, 10000);
         }
     }
+    지금보이는사진 = 지금보이는사진 + 1;
 });
 매니저2.on('pan', function(e){
+    // 지금보이는사진 = 1
     if( e.deltaX < -100){
         $('.slide-container').css('transform','translateX('+ e.deltaX +'px)');
 
@@ -92,6 +97,7 @@ var 매니저3 = new Hammer.Manager(이미지3);
             }, 10000);
         }
     }
+    // 지금보이는사진 = 지금보이는사진 + 1;
 });
 매니저2.on('pan', function(e){
     if( e.deltaX > 100){
